82ade7fa9ca453db972a370c6d1518d387bb9881
ggomma <ggomma@gmail.com>
2024-10-12 23:44:32 +0900

    feat: implement web3 provider update logic

--- src/evm.ts
+++ src/evm.ts
@@ -1,4 +1,8 @@
 import Web3 from 'web3'
 
-const rpcUrl = `https://api.avax.network/ext/bc/C/rpc`
-export const web3 = new Web3(rpcUrl)
+const defaultRpcUrl = `https://api.avax.network/ext/bc/C/rpc`
+
+export let web3 = new Web3(defaultRpcUrl)
+export const changeRpc = (rpcProvider: any) => {
+    web3 = new Web3(rpcProvider)
+}
