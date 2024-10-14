975458e9cb56df78e01325f2e2f046da9847ac1b
KAPUIST <91464689+KAPUIST@users.noreply.github.com>
2024-10-15 07:09:22 +0900

    fungible

--- src/components/wallet/portfolio/Fungibles.vue
+++ src/components/wallet/portfolio/Fungibles.vue
@@ -125,8 +125,10 @@ export default class Fungibles extends Vue {
 
     get erc20Balances(): Erc20Token[] {
         let tokens: Erc20Token[] = this.$store.getters['Assets/networkErc20Tokens']
+
         let filt = tokens.filter((token) => {
-            if (token.balanceBN.isZero()) return false
+            // 테스트용 입니다.
+            //if (token.balanceBN.isZero()) return false
             return true
         })
         return filt
