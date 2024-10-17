4f04a23f211315e1536a87b22ba7a1122f7a4715
ggomma <ggomma@gmail.com>
2024-10-17 14:17:48 +0900

    chore: change app name to vivalanche

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import { CapacitorConfig } from '@capacitor/cli'
 
 const config: CapacitorConfig = {
-    appId: 'com.example.app',
-    appName: 'wallet',
+    appId: 'com.vivalanche.app',
+    appName: 'vivalanche-wallet',
     webDir: 'dist',
     server: {
         androidScheme: 'https',
