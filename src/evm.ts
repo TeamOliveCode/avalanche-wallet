a99be780610364a3859f055c19e6a0683f7a95f1
ggomma <ggomma@gmail.com>
2024-11-19 00:09:32 +0900

    fix: use https domain

--- src/evm.ts
+++ src/evm.ts
@@ -1,6 +1,6 @@
 import Web3 from 'web3'
 
-const defaultRpcUrl = `http://13.124.212.163:9650/ext/bc/C/rpc`
+const defaultRpcUrl = `https://testnet-rpc.geeblock.io/ext/bc/C/rpc`
 
 export let web3 = new Web3(defaultRpcUrl)
 export const changeRpc = (rpcProvider: any) => {
--- src/store/modules/network/constants.ts
+++ src/store/modules/network/constants.ts
@@ -2,7 +2,7 @@ import { AvaNetwork } from '@/js/AvaNetwork'
 
 export const TestnetConfig = new AvaNetwork(
     'Testnet',
-    'http://13.124.212.163:9650',
+    'https://testnet-rpc.geeblock.io',
     90110,
     'http://3.38.103.197:3001',
     'http://3.38.103.197:8000',
