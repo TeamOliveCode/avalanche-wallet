d19028439fe15bc0be5d45b2a34c7f101b842889
ggomma <ggomma@gmail.com>
2024-11-18 16:33:29 +0900

    fix: use testnet chainId

--- src/store/modules/network/constants.ts
+++ src/store/modules/network/constants.ts
@@ -3,7 +3,7 @@ import { AvaNetwork } from '@/js/AvaNetwork'
 export const TestnetConfig = new AvaNetwork(
     'Testnet',
     'http://13.124.212.163:9650',
-    90107,
+    90110,
     'http://3.38.103.197:3001',
     'http://3.38.103.197:8000',
     true
